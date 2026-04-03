c84b7753d36aec28322fe68b7e5ff997573c43e5
fatimaezzahra99 <fe045001@gmail.com>
2026-04-03 20:05:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from './HelloWorld';
+import Profil from './Profil';
+import Voiture from './Voiture';
+import ListeCourses from './ListeCourses';
 
 function App() {
+  // Pour Profil
+  const utilisateur = { nom: 'Emma', photo: '/user.png' };
+
+  // Pour ListeCourses
+  const fruits = ['Pomme', 'Banane', 'Orange'];
+  const legumes = ['Carotte', 'Brocoli', 'Tomate'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TP React MLIAEdu</h1>
+
+      {/* Composant HelloWorld */}
+      <HelloWorld />
+
+      {/* Composant Profil */}
+      <Profil utilisateur={utilisateur} taille={150} />
+
+      <hr />
+
+      {/* Exercices Voiture */}
+      <h2>Exercice Voiture</h2>
+      <Voiture marque="Peugeot" modele="208" couleur="Rouge" />
+      <Voiture marque="Renault" modele="Clio" couleur="Bleu" />
+      <Voiture marque="Toyota" modele="Corolla" couleur="Blanc" />
+
+      <hr />
+
+      {/* Exercices ListeCourses */}
+      <h2>Exercice ListeCourses</h2>
+      <h3>Fruits</h3>
+      <ListeCourses elements={fruits} />
+
+      <h3>Légumes</h3>
+      <ListeCourses elements={legumes} />
     </div>
   );
 }
